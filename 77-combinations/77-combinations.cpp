ea245f51b7f1d52cde3d2fc0b49c86a6616a2243
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-17 04:56:16 +0200

    Time: 70 ms (27.51%), Space: 44.3 MB (23.49%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-void helper(int n, int index, int k, vector<int> output) {
-    output.push_back(index + 1);
-    if(k == 1) // index == n + 1
+void helper(int n, int index, int k, vector<int> output) {    
+    if(k == 0)
         return ans.push_back(output);
-    for(int i = index + 1; i < n; ++i) {
-        helper(n, i, k - 1, output);
-    }
+    if(index <= n - k)
+        helper(n, index + 1, k, output);
+    output.push_back(index);
+    helper(n, index + 1, k - 1, output);
 }
 vector<vector<int>> combine(int n, int k) {
-    for(int i = 0; i <= n - k; ++i)
-        helper(n, i, k, {});
+    helper(n, 1, k, {});
     return ans;
 }
 };
