3a3de2949fea1f5cf2503968dfcf48f24c4f316d
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-17 03:35:01 +0200

    Time: 4 ms (65.77%), Space: 8 MB (37.41%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,26 +1,18 @@
 class Solution {
-public:    
+public:
+    vector<vector<int>> res;
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<bool> taken(nums.size(), false);
-        vector<int> prefix;
-        return perm(nums, taken, prefix);
+        perm(nums, 0);
+        return res;
     }
 private:
-    vector<vector<int>> perm(vector<int> nums, vector<bool> taken, vector<int> prefix) {
-        vector<vector<int>> res;
-        if(nums.size() == prefix.size())
-            res.push_back(prefix);
-        for(int i = 0; i < nums.size(); i++) {
-            if(!taken[i]) {
-                prefix.push_back(nums[i]);
-                taken[i] = true;
-                vector<vector<int>> tmp = perm(nums, taken, prefix);
-                for(int i = 0; i < tmp.size(); i++)
-                    res.push_back(tmp[i]);
-                taken[i] = false;
-                prefix.pop_back();
-            }                        
+    void perm(vector<int>& nums, int index) {
+        if(index == nums.size())
+            return res.push_back(nums);
+        for(int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            perm(nums, index + 1);
+            swap(nums[index], nums[i]);
         }
-        return res;
     }
 };
