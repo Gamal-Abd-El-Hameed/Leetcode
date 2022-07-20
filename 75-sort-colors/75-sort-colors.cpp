7238d1d4258c468072a14554b1b6ac776d215bdd
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-20 22:48:04 +0200

    Time: 3 ms (62.94%), Space: 8.3 MB (70.19%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int z = 0, s = nums.size()-1;
-        for(int i = 0; i <= s; ++i) {
-            while(nums[i] == 2 && i < s)
-                swap(nums[i],nums[s--]);
-            while(nums[i]==0&&i>z)
-                swap(nums[i],nums[z++]);
+        int colors[3] = {0,0,0};
+        for(int x: nums)
+            ++colors[x];
+        int i = 0, c = 0;
+        for(int x: colors) {
+            while(x--) {
+                nums[i] = c;
+                ++i;
+            }
+            ++c;
         }
     }
 };
