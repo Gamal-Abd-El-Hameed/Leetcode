f200cedb448158a69ee7195716077be9eac3ac69
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-20 15:35:02 +0200

    Time: 246 ms (77.62%), Space: 31.5 MB (97.91%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    int bs(vector<int> &arr, int x){
-        int start = 0;
-        int end = arr.size()-1;
-        int ans = -1;
-        while (start <= end)
-        {
-            int mid = (start + end) / 2;
-
-            if (arr[mid] <= x)
-                start = mid + 1;
-            else
-            {
-                ans = mid;
-                end = mid - 1;
-            }
+    bool hasMatches(string &curr, string &s) {
+        int pos = 1;
+        int i = s.find(curr[0]);
+        if (i == -1)
+            return false;
+        while (pos < curr.length()) {
+            i = s.find(curr[pos], i + 1);
+            if (i == -1)
+                return false;
+            pos++;
         }
-        return ans==-1 ? ans : arr[ans];
+        return true;
     }
     
-
     int numMatchingSubseq(string s, vector<string>& words) {
-	
-		// First , we are mapping index of characters of given string to respective characters
-        unordered_map<char,vector<int>> mp;
-        for(int i=0;i<s.length();i++){
-            mp[s[i]].push_back(i);
-        }
-          
-        int count = words.size(); // initializing ans 
-		
-        for(auto w : words){
-            int prev = -1;
-            for(int j=0;j<w.size();j++){
-				// Searching for strictly greater element than prev using binary search
-                int x = bs(mp[w[j]],prev);
-				// If strictly greater element not found, the current subsequence cannot be formed.
-                if(x == -1){
-                    count--;
-                    break;
-                }
-				// Else, updating the prev
-                else{
-                    prev = x;
-                }
-            }
-        }
+        int count = 0;
+        for (string str : words)
+            if (hasMatches(str, s))
+                count++;
+        
         return count;
     }
 };
