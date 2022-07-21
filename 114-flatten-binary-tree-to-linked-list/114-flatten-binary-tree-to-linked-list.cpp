6f009ef8bd5f3bb8633ea06a3f848308e79f342a
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-21 20:43:30 +0200

    Time: 0 ms (100.00%), Space: 12.7 MB (78.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,17 +1,18 @@
 class Solution {
-public:void flatten(TreeNode *root) {
-	while (root) {
-		if (root->left && root->right) {
-			TreeNode* t = root->left;
-			while (t->right)
-				t = t->right;
-			t->right = root->right;
-		}
-
-        if(root->left)
-		    root->right = root->left;
-		root->left = NULL;
-		root = root->right;
-	}
-}
+public:
+    void flatten(TreeNode *root) {
+        TreeNode* t;
+        while(root) {
+            if(root->left && root->right) {
+                t = root->left;
+                while(t->right)
+                    t = t->right;
+                t->right = root->right;
+            }
+            if(root->left)
+                root->right = root->left;
+            root->left = NULL;
+            root = root->right;
+        }
+    }
 };
