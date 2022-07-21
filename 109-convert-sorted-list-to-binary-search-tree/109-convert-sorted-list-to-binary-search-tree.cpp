11ec22f385cb54543d2894286a6226a12813efa0
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-21 17:37:00 +0200

    Time: 19 ms (99.24%), Space: 28.4 MB (33.29%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,44 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    TreeNode* helper(unordered_map<int, ListNode*> &m, int start, int end) {
-        if(start > end)
-            return NULL;
-        int mid = (start + end) >> 1;
-        TreeNode* root = new TreeNode(m[mid]->val);
-        root->left = helper(m, start, mid - 1);
-        root->right = helper(m, mid + 1, end);
-        return root;
-    }
 public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        unordered_map<int, ListNode*> m;
-        ListNode* temp = head;
-        int i = 0;
-        while(temp) {
-            m[i] = temp;
-            temp = temp->next;
-            ++i;
-        }
-        return helper(m, 0, i - 1);
+    TreeNode *sortedListToBST(ListNode *head)
+    {
+    	return sortedListToBST( head, NULL );
+    }
+    
+private:
+    TreeNode *sortedListToBST(ListNode *head, ListNode *tail)
+    {
+    	if( head == tail )
+    		return NULL;
+    	if( head->next == tail )    // 
+    	{	
+    		TreeNode *root = new TreeNode( head->val );
+    		return root;
+    	}
+    	ListNode *mid = head, *temp = head;
+    	while( temp != tail && temp->next != tail )    // 寻找中间节点
+    	{
+    		mid = mid->next;
+    		temp = temp->next->next;
+    	}
+    	TreeNode *root = new TreeNode( mid->val );
+    	root->left = sortedListToBST( head, mid );
+    	root->right = sortedListToBST( mid->next, tail );
+    	return root;
     }
 };
