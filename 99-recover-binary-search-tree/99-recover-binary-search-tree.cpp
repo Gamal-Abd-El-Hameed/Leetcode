aaefbe198b69d0c9491fd02c73db2d1eac19015b
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-31 21:47:00 +0200

    Time: 38 ms (86.01%), Space: 57.9 MB (43.29%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,33 +1,28 @@
 class Solution {
-public:
-    void inor(TreeNode*&root,vector<TreeNode*>&v){
-        if(root==nullptr)return;
-        inor(root->left,v);
-        v.push_back(root);
-        inor(root->right,v);
-    }
-    void recoverTree(TreeNode* root) {
-        vector<TreeNode*>inorder;
-        inor(root,inorder);
-        int first=-1,second=-1,third=-1;
-        for(int i=0;i<inorder.size()-1;i++){
-            if(inorder[i]->val>inorder[i+1]->val){
-                if(first==-1&&second==-1){
-                first=i;
-                second=i+1;
-                }
-                else{third=i+1;}
-            }
+    TreeNode* first;
+    TreeNode* second;
+    TreeNode* pre;
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        
+        if(first==NULL && (pre==NULL ||pre->val>=root->val)){
+            first = pre;
         }
-            if(third==-1){
-                int temp=inorder[first]->val;
-                inorder[first]->val=inorder[second]->val;
-                inorder[second]->val=temp;
-            }
-            else{
-                 int temp=inorder[first]->val;
-                inorder[first]->val=inorder[third]->val;
-                inorder[third]->val=temp;
-                }
-             }
+        if(first!=NULL && pre->val>=root->val){
+            second = root;
+        }
+        pre = root;
+        inorder(root->right);
+    }
+    public:void recoverTree(TreeNode* root) {
+        if(root==NULL) return;
+        first = NULL;
+        second = NULL;
+        pre = NULL;
+        inorder(root);
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
+    }    
 };
