8e04b76ca1659f19c251b4b2c6b19568a1d6f560
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-07-31 21:47:40 +0200

    Time: 43 ms (77.44%), Space: 57.9 MB (70.10%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -2,16 +2,14 @@ class Solution {
     TreeNode* first;
     TreeNode* second;
     TreeNode* pre;
-    void inorder(TreeNode* root){
+    void inorder(TreeNode* root) {
         if(root==NULL) return;
         inorder(root->left);
         
-        if(first==NULL && (pre==NULL ||pre->val>=root->val)){
-            first = pre;
-        }
-        if(first!=NULL && pre->val>=root->val){
-            second = root;
-        }
+        if(first==NULL && (pre==NULL ||pre->val>=root->val))
+            first = pre;        
+        if(first!=NULL && pre->val>=root->val)
+            second = root;        
         pre = root;
         inorder(root->right);
     }
