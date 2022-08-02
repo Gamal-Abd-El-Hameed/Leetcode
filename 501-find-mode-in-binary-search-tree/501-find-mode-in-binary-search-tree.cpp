364ace904f3d9285c4f35b26e022161370594a8c
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-08-02 17:57:04 +0200

    Time: 31 ms (63.11%), Space: 24 MB (92.92%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -1,39 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-unordered_map<int, int> m;
-void inorder(TreeNode* root) {
-    if(root == NULL)
-        return;
-    inorder(root->left);
-    int val = root->val;
-    if(m.count(val))
-        ++m[val];
-    else
-        m.insert({val, 1});
-    inorder(root->right);
-}
+public:
+    int maxFreq = 0, currFreq = 0, precursor = INT_MIN;
+    vector<int> res;
 
-public:vector<int> findMode(TreeNode* root) {
-    if(root == NULL)
-        return {};
-    vector<int> ans;
-    inorder(root);
-    int maxElem = 0;
-    for(auto n : m)
-        maxElem = max(maxElem, n.second);
-    for(auto n: m)
-        if(n.second == maxElem)
-            ans.push_back(n.first);
-    return ans;
-}
+    vector<int> findMode(TreeNode *root)
+    {
+        inorderTraversal(root);
+        return res;
+    }
+
+    void inorderTraversal(TreeNode *root)
+    {
+        if (root == NULL) return; // Stop condition
+        inorderTraversal(root->left); // Traverse left subtree
+        if (precursor == root->val) currFreq++;
+        else currFreq = 1;
+        if (currFreq > maxFreq)
+        {// Current node value has higher frequency than any previous visited
+            res.clear();
+            maxFreq = currFreq;
+            res.push_back(root->val);
+        }
+        else if (currFreq == maxFreq)
+        {// Current node value has a frequency equal to the highest of previous visited
+            res.push_back(root->val);
+        }
+        precursor = root->val; // Update the precursor
+        inorderTraversal(root->right); // Traverse right subtree
+    }
 };
