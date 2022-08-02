187b172576a89343326a0cedd8ead07ac26a295f
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-08-02 18:06:25 +0200

    Time: 31 ms (63.11%), Space: 24.1 MB (80.61%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -1,31 +1,24 @@
 class Solution {
-public:
     int maxFreq = 0, currFreq = 0, precursor = INT_MIN;
-    vector<int> res;
-
-    vector<int> findMode(TreeNode *root)
-    {
-        inorderTraversal(root);
-        return res;
-    }
-
-    void inorderTraversal(TreeNode *root)
-    {
-        if (root == NULL) return; // Stop condition
-        inorderTraversal(root->left); // Traverse left subtree
-        if (precursor == root->val) currFreq++;
+    vector<int> ans;
+    void inorder(TreeNode* root) {
+        if(root == NULL)
+            return;
+        inorder(root->left);
+        if(precursor == root->val) ++currFreq;
         else currFreq = 1;
-        if (currFreq > maxFreq)
-        {// Current node value has higher frequency than any previous visited
-            res.clear();
+        if(currFreq > maxFreq) {
             maxFreq = currFreq;
-            res.push_back(root->val);
+            ans.clear();
+            ans.push_back(root->val);
         }
-        else if (currFreq == maxFreq)
-        {// Current node value has a frequency equal to the highest of previous visited
-            res.push_back(root->val);
-        }
-        precursor = root->val; // Update the precursor
-        inorderTraversal(root->right); // Traverse right subtree
+        else if(currFreq == maxFreq)
+            ans.push_back(root->val);
+        precursor = root->val;
+        inorder(root->right);
+    }
+    public: vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return ans;
     }
 };
