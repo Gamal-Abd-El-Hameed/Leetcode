82b0930effc681967b2df209ead07647dd1daba3
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-08-06 02:21:42 +0200

    Time: 262 ms (88.97%), Space: 69.5 MB (60.01%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,29 +1,21 @@
-class Solution {
-    class Compare {
+class NumStrMinComparator {
 public:
-    bool operator()(const string &a, const string& b) {
-        if(a.size() != b.size())
-            return a.size() > b.size();
-        return a > b;
+    bool operator() (string &b, string &a) { // Please note, I swap position of `a` and `b` here!
+        if (a.size() != b.size()) return a.size() < b.size();
+        return  a < b;
     }
 };
 
-bool compare (const string &a, const string& b) {
-    if(a.size() != b.size())
-        return a.size() > b.size();
-    return a > b;
-}
-
-public:string kthLargestNumber(vector<string>& nums, int k) {
-    priority_queue<string, vector<string>, Compare> heap;
-    for(int i = 0; i < k; ++i)
-        heap.push(nums[i]);
-    for(int i = k; i < nums.size(); ++i) {
-        if(compare(nums[i], heap.top())) {
-            heap.pop();
-            heap.push(nums[i]);
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        priority_queue<string, vector<string>, NumStrMinComparator> minHeap;
+        for (string& num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
         }
+        return minHeap.top();
     }
-    return heap.top();
-}
 };
