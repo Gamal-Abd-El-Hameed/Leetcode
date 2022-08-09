2c215defc94f9c6518d14b01076ad5479b9ae36c
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-08-09 20:09:06 +0200

    Time: 33 ms (71.90%), Space: 13.3 MB (75.86%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    struct compare {
-    bool operator()(const ListNode* l, const ListNode* r) {
-        return l->val > r->val;
+    static bool heapComp(ListNode* a, ListNode* b) {
+        return a->val > b->val;
+}
+ListNode* mergeKLists(vector<ListNode*>& lists) { //make_heap
+    ListNode head(0);
+    ListNode *curNode = &head;
+    vector<ListNode*> v;   
+    for(int i =0; i<lists.size(); i++){
+        if(lists[i]) v.push_back(lists[i]);
     }
-};
-ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
-    priority_queue<ListNode *, vector<ListNode *>, compare> q;
-    for(auto l : lists) {
-        if(l)  q.push(l);
-    }
-    if(q.empty())  return NULL;
+    make_heap(v.begin(), v.end(), heapComp); //vector -> heap data strcture
 
-    ListNode* result = q.top();
-    q.pop();
-    if(result->next) q.push(result->next);
-    ListNode* tail = result;            
-    while(!q.empty()) {
-        tail->next = q.top();
-        q.pop();
-        tail = tail->next;
-        if(tail->next) q.push(tail->next);
+    while(v.size()>0){
+        curNode->next=v.front();
+        pop_heap(v.begin(), v.end(), heapComp); 
+        v.pop_back(); 
+        curNode = curNode->next;
+        if(curNode->next) {
+            v.push_back(curNode->next); 
+            push_heap(v.begin(), v.end(), heapComp);
+        }
     }
-    return result;
+    return head.next;
 }
 };
