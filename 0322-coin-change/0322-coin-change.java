7060f66370b600c16dc973ccd5a14d61d3a8f9e3
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-11 01:18:48 +0200

    Time: 44 ms (35.90%), Space: 41.8 MB (95.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,7 +5,6 @@ public class Solution {
   }
 
   private int coinChange(int[] coins, int rem, int[] count) {
-    if (rem < 0) return -1;
     if (rem == 0) return 0;
     if (count[rem - 1] != 0) return count[rem - 1];
     int min = Integer.MAX_VALUE;
