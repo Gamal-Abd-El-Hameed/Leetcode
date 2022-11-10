7f65e7e3db501c7722c27eb531583309f35cf621
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-11 01:25:02 +0200

    Time: 62 ms (23.35%), Space: 45.2 MB (46.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,12 +1,15 @@
 public class Solution {
 
   public int coinChange(int[] coins, int amount) {
-    return coinChange(coins, amount, new int[amount]);
+    return coinChange(coins, amount, new int[amount + 1]);
   }
 
   private int coinChange(int[] coins, int amount, int[] dp) {
-    if (amount == 0) return 0;    
-    if (dp[amount - 1] != 0) return dp[amount - 1];
+    if (amount == 0) {
+        dp[0] = 0;
+        return 0;
+    }
+    if (dp[amount] != 0) return dp[amount];
     int min = Integer.MAX_VALUE;
     int diff;
     for (int coin : coins) {
@@ -17,7 +20,7 @@ public class Solution {
       if (res >= 0 && res < min)
         min = 1 + res;
     }
-    dp[amount - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
-    return dp[amount - 1];
+    dp[amount] = (min == Integer.MAX_VALUE) ? -1 : min;
+    return dp[amount];
   }
 }
