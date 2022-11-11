07f14cb5487dd6aa98830073e451d3f5396877dc
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-11 16:17:16 +0200

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -48,20 +48,23 @@ class gfg
 
 class Solution 
 { 
-    //Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[], int n) 
-   {
-	int []dp = new int[W + 1];
-	for (int i = 0; i < n; i++) {
-	for (int w = W; w >= 0; w--) {
-		if (wt[i] > w)		
-			break;
-		dp[w] = Math.max(dp[w],
-						dp[w - wt[i]] + val[i]);
+   static int max(int a, int b) { return (a > b) ? a : b;}
+
+	static int knapSackRec(int W, int wt[], int val[], int n, int [][]dp) {	
+		if (n == 0 || W == 0) return 0;		
+		if (dp[n][W] != -1) return dp[n][W];	
+		if (wt[n - 1] > W) return dp[n][W] = knapSackRec(W, wt, val, n - 1, dp);										
+		return dp[n][W] = max((val[n - 1] + knapSackRec(W - wt[n - 1], wt, val, n - 1, dp)),
+							knapSackRec(W, wt, val, n - 1, dp));			
 	}
+
+	static int knapSack(int W, int wt[], int val[], int n) {
+		int dp[][] = new int[n + 1][W + 1];	
+		for(int i = 0; i <= n; i++)
+			for(int j = 0; j <= W; j++)
+				dp[i][j] = -1;	
+		return knapSackRec(W, wt, val, n, dp);	
 	}
-	return dp[W];
-}
 }
 
 
