eeee8eb16cd01a62cdb2ffb06957ef07e3a9f085
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-12 01:24:33 +0200

    Time: 153 ms (60.54%), Space: 14.6 MB (55.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount + 1, INT_MAX - 1);
         dp[0] = 0;
         for(int coin: coins)
             if(coin <= amount)
@@ -10,10 +10,10 @@ public:
         for(int i = 1; i <= amount; ++i) {
             for(int coin: coins) {
                 diff = i - coin;
-                if(diff >= 0 && dp[diff] != INT_MAX)
+                if(diff >= 0)
                     dp[i] = min(dp[i], 1 + dp[diff]);
             }
         }
-        return (dp[amount] == INT_MAX) ? -1: dp[amount];
+        return (dp[amount] >= INT_MAX - 1) ? -1: dp[amount];
     }
 };
