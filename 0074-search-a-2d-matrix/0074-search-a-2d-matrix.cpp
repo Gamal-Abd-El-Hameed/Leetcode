bf5f4aeabaff9a3040cb5382ab5b821fb0606d7a
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-11-18 23:10:24 +0200

    Time: 8 ms (55.14%), Space: 9.6 MB (5.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
-			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
-        }
-        
-        return false;
-    }
+    bool searchMatrixRec(vector<vector<int>>& matrix, int target, int low, int high) {
+    if (low == high)
+        return binary_search(matrix[low].begin(), matrix[low].end(), target);
+    int mid = low + (high - low) / 2, col = matrix[0].size() - 1;
+    if (matrix[mid][col] >= target)
+        return searchMatrixRec(matrix, target, low, mid);
+    return searchMatrixRec(matrix, target, mid + 1, high);    
+}
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    return searchMatrixRec(matrix, target, 0, matrix.size() - 1);
+}
 };
