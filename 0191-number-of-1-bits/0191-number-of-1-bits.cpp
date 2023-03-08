6ed9584b34f37945648639b79231baa028bb30b3
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-03-08 21:44:53 +0200

    Time: 3 ms (42.45%), Space: 6 MB (36.66%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
-        while(n > 0) {
-            if (n)
-                count++;
+        while(n) {
+            count++;
             n = n & (n - 1);            
         }
         return count;
