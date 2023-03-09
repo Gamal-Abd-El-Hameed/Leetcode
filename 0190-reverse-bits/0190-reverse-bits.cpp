b44b364bfdea3a4de0a52bffc83618c7b834cfdc
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-03-09 02:32:32 +0200

    Time: 0 ms (100.00%), Space: 6 MB (37.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,16 +3,11 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
         int count = 32;
-        while(n) {
+        while(count--) {
             ans <<= 1;
             if (n & 1)
                 ans |= 1;
             n >>= 1;
-            count--;
-        }
-        while(count) {
-            count--;
-            ans <<= 1;
         }
         return ans;
     }
