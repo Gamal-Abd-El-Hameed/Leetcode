b034cce6772ddfcf1396e1225034e48bd8652fec
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-03-09 02:06:56 +0200

    Time: 16 ms (88.66%), Space: 18 MB (25.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int ans = 0;
+        int n = nums.size();
+        int ans = (n * (n + 1)) >> 1;
         for (int num: nums)
-            ans ^= num;
-        for (int i = 0; i <= nums.size(); i++)
-            ans ^= i;
+            ans -= num;
         return ans;
     }
 };
