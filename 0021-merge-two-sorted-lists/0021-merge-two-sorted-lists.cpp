7ca2e34aeb86e4ab3c345f9d9c5ee57efb61854d
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-04-11 00:58:59 +0200

    Time: 12 ms (31.50%), Space: 14.9 MB (50.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,32 +11,23 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr)
-            return list2;
-        if (list2 == nullptr)
-            return list1;
-        auto* curr = new ListNode(0);
-        ListNode* head = curr;
-        while (true) {
-            if (list1->val <= list2->val) {
-                curr->val = list1->val;
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
                 list1 = list1->next;
-                if (list1 == nullptr) {
-                    curr->next = list2;
-                    break;
-                }
-            }
-            else {
-                curr->val = list2->val;
+            } else {
+                tail->next = list2;
                 list2 = list2->next;
-                if (list2 == nullptr) {
-                    curr->next = list1;
-                    break;
-                }
             }
-            curr->next = new ListNode(0);
-            curr = curr->next;
+            tail = tail->next;
         }
-        return head;
+        if (list1) {
+            tail->next = list1;
+        } else if (list2) {
+            tail->next = list2;
+        }
+        return dummy->next; 
     }
 };
