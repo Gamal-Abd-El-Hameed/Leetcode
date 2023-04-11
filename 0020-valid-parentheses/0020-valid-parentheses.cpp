7caa0a9f2e369b953f485b7585611bc84191ec87
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-04-11 02:04:30 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (56.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
+    if (s.length() % 2) return false;
     stack<char> st;
     const char openingBrackets[] = {'(', '{', '['};
     const char closingBrackets[] = {')', '}', ']'};
