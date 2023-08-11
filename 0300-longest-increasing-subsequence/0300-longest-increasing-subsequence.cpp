4318730c1a5847509218e8efe1371201cd9882a7
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-11 12:20:19 +0300

    Time: 281 ms (67.66%), Space: 10.6 MB (51.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,14 +5,12 @@ public:
         if (n <= 1) return n;
         vector<int> dp(n, 1);        
         int maxLength = 1;        
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++) 
                 if (nums[i] > nums[j]) {
                     dp[i] = max(dp[i], dp[j] + 1);
                     maxLength = max(maxLength, dp[i]);
-                }
-            }
-        }
+                }              
         
         return maxLength;
     }
