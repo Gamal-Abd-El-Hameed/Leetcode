69f222d2059c4eaa25f5dfc12153c0e77b9a3d1b
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-11 22:32:52 +0300

    Time: 0 ms (100.00%), Space: 8.8 MB (72.05%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,19 +1,23 @@
+
 class Solution {
 private:
-    bool helper(string s, int i, int j, vector<string>& wordDict) {
-        if (dp[i][j] != -1) return dp[i][j];
-        int n = s.size();
-        string sub = s.substr(i, j - i + 1);
-        bool found = find(wordDict.begin(), wordDict.end(), sub) != wordDict.end();
-        if (j == n - 1) return dp[i][j] = found;
-        if (found)
-            return dp[i][j] = helper(s, i, j + 1, wordDict) || helper(s, j + 1, j + 1, wordDict);
-        return dp[i][j] = helper(s, i, j + 1, wordDict);
+    bool helper(string s, vector<string>& wordDict, int i) {
+        if (i == s.size()) return true;
+        if (dp[i] != -1) return dp[i];
+        string subsetString;
+        for (const string& word : wordDict) {
+            subsetString = s.substr(i, word.size());
+            if (word == subsetString) {
+                dp[i] = helper(s, wordDict, i + word.size());
+                if (dp[i]) return true;
+            }
+        }
+        return dp[i] = false;
     }
 public:
-    vector<vector<int>> dp;
+    vector<int> dp;
     bool wordBreak(string s, vector<string>& wordDict) {
-        dp = vector<vector<int>>(s.size(), vector<int>(s.size(), -1));
-        return helper(s, 0, 0, wordDict);
+        dp.resize(s.size(), -1);
+        return helper(s, wordDict, 0);
     }
 };
