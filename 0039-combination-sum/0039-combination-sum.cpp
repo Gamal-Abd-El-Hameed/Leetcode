58776998f76d650956f2f0ebccc16006451c3d97
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-11 23:18:01 +0300

    Time: 24 ms (31.39%), Space: 15 MB (39.78%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<vector<int>>> dp(target + 1);
+        dp[0].push_back(vector<int>());
+        vector<vector<int>> sub;
+        for (int i = 1; i <= target; i++) {
+            for (int candidate : candidates) {
+                if (candidate <= i) {
+                    sub = dp[i - candidate];
+                    if (!sub.empty()) {
+                        for (vector<int> &v : sub) {
+                            v.insert(upper_bound(v.begin(), v.end(), candidate), candidate);
+                            if (find(dp[i].begin(), dp[i].end(), v) == dp[i].end()) {
+                                dp[i].push_back(v);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[target];
+    }
+};
