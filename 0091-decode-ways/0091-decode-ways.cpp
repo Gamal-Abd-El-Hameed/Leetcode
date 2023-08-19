e60ef18afc567fc8d55072fa3b78b63b072d5508
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-19 20:46:44 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (45.85%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    int n;
-    vector<int>dp;
-    bool isStringFrom10To26(string s) {
-        return stoi(s) <= 26;
-    }
-    int helper(string s, int i) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-        if (s[i] == '0') return dp[i] = 0;
-        if (i == n - 1) return dp[i] = 1;
-        int firstTaken = helper(s, i + 1);
-        if (!isStringFrom10To26(s.substr(i, 2))) return dp[i] = firstTaken;
-        int secondTaken;
-        if (i == n - 2) secondTaken = 1;
-        else secondTaken = helper(s, i + 2);
-        return dp[i] = firstTaken + secondTaken;
-    }
 public:
     int numDecodings(string s) {
-        n = s.length();
-        dp.resize(n, -1);
-        return helper(s, 0);
+        if (s[0] == '0') {
+            return 0;
+        }
+        
+        int n = s.size();
+        
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            int ones = stoi(s.substr(i - 1, 1));
+            if (ones >= 1 && ones <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            int tens = stoi(s.substr(i - 2, 2));
+            if (tens >= 10 && tens <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
     }
 };
