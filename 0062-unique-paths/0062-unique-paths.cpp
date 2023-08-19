cc024ca8a064f8e5d5d284eb0f3ff07f8437c7f1
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-19 21:23:59 +0300

    Time: 0 ms (100.00%), Space: 6.6 MB (5.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,13 @@
 class Solution {
-private:
-    int rows, cols;
-    vector<vector<int>> dp;
-    int helper(int i, int j) {
-        if (i == rows || j == cols) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        if (i == rows - 1 && j == cols - 1) return dp[i][j] = 1;
-        return dp[i][j] = helper(i + 1, j) + helper(i, j + 1);
-    }
 public:
     int uniquePaths(int m, int n) {
-        rows = m, cols = n;
-        dp = vector<vector<int>>(rows, vector<int>(cols, -1));
-        return helper(0, 0);
+        vector<vector<int>>dp(m + 1, vector<int>(n + 1, 0));
+        dp[0][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
     }
 };
