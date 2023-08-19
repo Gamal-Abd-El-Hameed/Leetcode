447a4b8235aa3476fca2de7be2db6191e21ff7b7
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-19 22:10:31 +0300

    Time: 2 ms (44.07%), Space: 6.1 MB (70.60%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<int> row(n, 1);
-        vector<int> newRow(n, 1);
+        vector<int> newRow(n);
         for (int i = 0; i < m - 1; i++) {
-            fill(newRow.begin(), newRow.end(),1);
+            newRow[n - 1] = 1;
             for (int j = n - 2; j >= 0; j--)
                 newRow[j] = newRow[j + 1] + row[j];
             row = newRow;
