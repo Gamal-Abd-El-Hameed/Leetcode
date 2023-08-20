d64105bc9baee32bcc97fae39c5425293851b644
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-20 10:53:54 +0300

    Time: 945 ms (13.34%), Space: 48.7 MB (20.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,16 @@
 class Solution {
-private:
-    int n;
-    vector<int>dp;
-    bool helper(vector<int>& nums, int i) {
-        if (i == n) return false;
-        if (i == n - 1) return dp[i] = true;
-        if (dp[i] != -1) return dp[i];
-        int jumps = nums[i];
-        for (int jump = 1; jump <= jumps; jump++) {
-            bool trial = helper(nums, i + jump);
-            if (trial) return dp[i] = true;
-        }
-        return dp[i] = false;
-    }
 public:
     bool canJump(vector<int>& nums) {
-        n = nums.size();
-        dp.resize(n, -1);
-        return helper(nums, 0);
+        int n = nums.size();
+        vector<bool>dp(n, false);
+        dp[0] = true;
+        int jumps;
+        for (int i = 1; i <= n; i++) {
+            if (!dp[i - 1]) continue;
+            jumps = nums[i - 1];
+            for (int jump = 1; jump <= jumps && i - 1 + jump < n; jump++)
+                dp[i - 1 + jump] = true;
+        }
+        return dp[n - 1];
     }
 };
