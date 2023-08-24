f595bf33036bae56c8f338742f5c08b2e24211be
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-24 17:44:02 +0300

    Time: 25 ms (52.47%), Space: 18 MB (5.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,23 +1,19 @@
 class Solution {
 private:
-    unordered_set<int> done;
     unordered_map<int, unordered_set<int>> valuesMap;
     unordered_set<int> visited;
     bool dfs(int key) {
-        if (done.find(key) != done.end()) return true;
+        if (visited.find(key) != visited.end()) return false;
+        if (valuesMap[key].empty()) return true;
         visited.insert(key);
-        for (int neighbor:valuesMap[key]) {
-            if (valuesMap.find(neighbor) == valuesMap.end()) continue;
-            if (visited.find(neighbor) != visited.end()) return false;
+        for (int neighbor:valuesMap[key])
             if (!dfs(neighbor)) return false;
-        }
         visited.erase(key);
-        done.insert(key);
+        valuesMap[key].clear();
         return true;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        valuesMap.reserve(numCourses);
         for (vector<int> pair:prerequisites)
             valuesMap[pair[0]].emplace(pair[1]);
         return all_of(valuesMap.begin(), valuesMap.end(),
