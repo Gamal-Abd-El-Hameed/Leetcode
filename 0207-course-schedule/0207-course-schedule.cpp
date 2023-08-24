5fe5380b49f48b01adb96e5a4d1185fb417efc52
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-24 16:28:21 +0300

    Time: 25 ms (52.47%), Space: 18.6 MB (5.09%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    unordered_set<int> done;
+    unordered_map<int, unordered_set<int>> valuesMap;
+    unordered_set<int> visited;
+    bool dfs(int key) {
+        if (done.find(key) != done.end()) return true;
+        visited.insert(key);
+        for (int neighbor:valuesMap[key]) {
+            if (valuesMap.find(neighbor) == valuesMap.end()) continue;
+            if (visited.find(neighbor) != visited.end()) return false;
+            if (!dfs(neighbor)) return false;
+        }
+        visited.erase(key);
+        done.insert(key);
+        return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (vector<int> pair:prerequisites)
+            valuesMap[pair[0]].insert(pair[1]);
+        return all_of(valuesMap.begin(), valuesMap.end(),
+                                        [this](const auto &pair)
+                                        { return dfs(pair.first); });
+
+    }
+};
