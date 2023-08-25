ea3090cfde546f819e2c976c12b018efa848d50e
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-26 00:48:45 +0300

    Time: 10 ms (83.82%), Space: 13.7 MB (8.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
-        vector<bool> rowsBool(rows, false);
+        unordered_set<int> rowsSet, colsSet;
         vector<bool> colsBool(cols, false);
         for (int row = 0; row < rows; row++)
             for (int col = 0; col < cols; col++)
                 if (matrix[row][col] == 0)
-                    rowsBool[row] = colsBool[col] = true;
-        for (int row = 0; row < rows; row++)
-            if (rowsBool[row])
-                for (int col = 0; col < cols; col++)
-                    matrix[row][col] = 0;
-        for (int col = 0; col < cols; col++)
-            if (colsBool[col])
-                for (int row = 0; row < rows; row++)
-                    matrix[row][col] = 0;
+                    rowsSet.insert(row), colsSet.insert(col);
+        // loop over rowsSet and colsSet
+        for (const auto &row : rowsSet)
+            for (int col = 0; col < cols; col++)
+                matrix[row][col] = 0;
+        for (const auto &col : colsSet)
+            for (int row = 0; row < rows; row++)
+                matrix[row][col] = 0;
     }
 };
