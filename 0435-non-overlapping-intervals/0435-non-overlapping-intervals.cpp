a229f08781ddd619ac39b9bc4593af5accbb667a
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-25 20:06:53 +0300

    Time: 330 ms (95.58%), Space: 89.7 MB (54.05%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;
-        // sort on end time
+        if (intervals.empty()) {
+            return 0;
+        }
+
+        // Sort intervals by their end points
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
         });
-        int removedIntervals = 0;
-        vector<int> currentInterval, lastInterval;
-        lastInterval = intervals[0];
-        for (int i = 1; i < intervals.size(); i++) {
-            currentInterval = intervals[i];
-            if (currentInterval[0] < lastInterval[1]) {
-                removedIntervals++;
+
+        int count = 1; // Count of non-overlapping intervals
+        int prevEnd = intervals[0][1]; // End point of the first interval
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] >= prevEnd) {
+                // Non-overlapping interval found, update count and prevEnd
+                ++count;
+                prevEnd = intervals[i][1];
             }
-            else
-                lastInterval = currentInterval;
         }
-        return removedIntervals;
+
+        // Total number of intervals minus non-overlapping intervals gives the answer
+        return intervals.size() - count;
     }
 };
