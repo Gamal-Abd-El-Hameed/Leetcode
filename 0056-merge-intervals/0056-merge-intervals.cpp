eca7368add4387527800784d440ed4ae1ec95c84
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-25 19:19:41 +0300

    Time: 29 ms (81.93%), Space: 19 MB (44.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) {
-            return {};
-        }
-
-        // Sort intervals based on their start times
+        if (intervals.size() <= 1) return intervals;
         sort(intervals.begin(), intervals.end());
-
-        vector<vector<int>> mergedIntervals;
-        mergedIntervals.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (intervals[i][0] <= mergedIntervals.back()[1]) {
-                // Merge overlapping intervals
-                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], intervals[i][1]);
-            } else {
-                // Add non-overlapping interval to the result
-                mergedIntervals.push_back(intervals[i]);
+        vector<vector<int>> res;
+        vector<int> currentInterval, newInterval;
+        newInterval = intervals[0];
+        for (int i = 1; i < intervals.size(); i++) {
+            currentInterval = intervals[i];
+            if (currentInterval[0] <= newInterval[1]) {
+                newInterval[1] = max(newInterval[1], currentInterval[1]);
+                if (i == intervals.size() - 1)
+                    res.push_back(newInterval);
+            }
+            else {
+                res.push_back(newInterval);
+                newInterval = currentInterval;
+                if (i == intervals.size() - 1)
+                    res.push_back(currentInterval);
             }
         }
-
-        return mergedIntervals;
+        return res;
     }
 };
