5cc09a7967ff1d079fc7ce50e1696d028a6528e6
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-26 01:54:57 +0300

    Time: 10 ms (83.82%), Space: 13.3 MB (53.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,24 +2,36 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
-        bool firstRow = false;
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    if (i == 0) firstRow = true;
-                    else matrix[i][0] = 0;
-                }
-        for (int i = 1; i < rows; i++)
+        bool setFirstRowToZero = false, setFirstColToZero = false;
+        for (int i = 0; i < rows; ++i) {
+            if (matrix[i][0] == 0) {
+                setFirstColToZero = true;
+                break;
+            }
+        }
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[0][j] == 0) {
+                setFirstRowToZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < rows; ++i)
+            for (int j = 1; j < cols; ++j)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        for (int i = 1; i < rows; ++i)
             if (matrix[i][0] == 0)
-                for (int j = 0; j < cols; j++)
+                for (int j = 1; j < cols; ++j)
                     matrix[i][j] = 0;
-        for (int j = 0; j < cols; j++)
+        for (int j = 1; j < cols; ++j)
             if (matrix[0][j] == 0)
-                for (int i = 0; i < rows; i++)
+                for (int i = 1; i < rows; ++i)
                     matrix[i][j] = 0;
-        if (firstRow)
-            for (int j = 0; j < cols; j++)
+        if (setFirstRowToZero)
+            for (int j = 0; j < cols; ++j)
                 matrix[0][j] = 0;
+        if (setFirstColToZero)
+            for (int i = 0; i < rows; ++i)
+                matrix[i][0] = 0;
     }
 };
