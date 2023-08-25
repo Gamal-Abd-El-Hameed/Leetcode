07ef0000a8bcb54e5ea3349f02eb11ef6f3e0673
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-25 19:07:09 +0300

    Time: 36 ms (59.88%), Space: 19.1 MB (44.49%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() <= 1) return intervals;
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        vector<int> currentInterval, newInterval;
+        newInterval = intervals[0];
+        for (int i = 1; i < intervals.size(); i++) {
+            currentInterval = intervals[i];
+            if (currentInterval[0] <= newInterval[1]) {
+                newInterval[0] = min(newInterval[0], currentInterval[0]);
+                newInterval[1] = max(newInterval[1], currentInterval[1]);
+                if (i == intervals.size() - 1)
+                    res.push_back(newInterval);
+            }
+            else {
+                res.push_back(newInterval);
+                newInterval = currentInterval;
+                if (i == intervals.size() - 1)
+                    res.push_back(currentInterval);
+            }
+        }
+        return res;
+    }
+};
