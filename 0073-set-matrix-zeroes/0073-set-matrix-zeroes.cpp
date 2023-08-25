fa970e29ac034deb8a94edea6510a04de515e01e
Gamal Abdel Hamid <es-gamalabdelhameed314@alexu.edu.eg>
2023-08-26 01:11:22 +0300

    Time: 16 ms (46.33%), Space: 14 MB (8.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
-        unordered_set<int> rowsSet, colsSet;
-        vector<bool> colsBool(cols, false);
-        for (int row = 0; row < rows; row++)
-            for (int col = 0; col < cols; col++)
-                if (matrix[row][col] == 0)
-                    rowsSet.insert(row), colsSet.insert(col);
-        // loop over rowsSet and colsSet
-        for (const auto &row : rowsSet)
-            for (int col = 0; col < cols; col++)
-                matrix[row][col] = 0;
-        for (const auto &col : colsSet)
-            for (int row = 0; row < rows; row++)
-                matrix[row][col] = 0;
+        vector<vector<int>> copyMatrix = matrix;
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                if (copyMatrix[i][j] == 0)
+                    for (j = 0; j < cols; j++)
+                        matrix[i][j] = 0;
+        for (int j = 0; j < cols; j++)
+            for (int i = 0; i < rows; i++)
+                if (copyMatrix[i][j] == 0)
+                    for (i = 0; i < rows; i++)
+                        matrix[i][j] = 0;
     }
 };
