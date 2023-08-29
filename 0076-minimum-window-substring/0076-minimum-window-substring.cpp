4ea016289a5e02c73321c91d8f9a688790f61454
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-08-30 00:22:57 +0300

    Time: 20 ms (61.46%), Space: 7.9 MB (80.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> targetChars(128, 0); // Store frequency of characters in t
-        
+        unordered_map<char, int> charFreq;
+
         for (char c : t) {
-            targetChars[c]++;
+            charFreq[c]++;
         }
-        
-        int requiredChars = t.size(); // Number of unique characters in t
-        
-        int left = 0, right = 0; // Sliding window pointers
-        int minLen = INT_MAX; // Minimum window length
-        int minStart = 0; // Start index of the minimum window
-        
-        int formed = 0; // Number of unique characters formed in the current window
-        vector<int> windowChars(128, 0); // Store frequency of characters in the current window
-        
+
+        int requiredChars = t.size(), left = 0, right = 0, minLen = INT_MAX, minStart = 0, formed = 0;
+        char c, leftChar;
         while (right < s.size()) {
-            char c = s[right];
-            windowChars[c]++;
-            if (windowChars[c] <= targetChars[c]) {
-                formed++;
+            c = s[right];
+            if (charFreq.count(c)) {
+                if (charFreq[c] > 0) {
+                    formed++;
+                }
+                charFreq[c]--;
             }
-            
+
             while (formed == requiredChars && left <= right) {
-                // Update the minimum window length and start index
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     minStart = left;
                 }
-                
-                char leftChar = s[left];
-                windowChars[leftChar]--;
-                if (windowChars[leftChar] < targetChars[leftChar]) {
-                    formed--;
+
+                leftChar = s[left];
+                if (charFreq.count(leftChar)) {
+                    if (charFreq[leftChar] >= 0) {
+                        formed--;
+                    }
+                    charFreq[leftChar]++;
                 }
-                
+
                 left++;
             }
-            
+
             right++;
         }
-        
+
         if (minLen == INT_MAX) {
             return "";
         }
-        
+
         return s.substr(minStart, minLen);
     }
 };
