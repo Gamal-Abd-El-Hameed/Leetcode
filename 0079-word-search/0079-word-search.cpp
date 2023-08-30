67cd62de2990569ef1461b572da78f2feb73415c
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-08-31 00:38:01 +0300

    Time: 251 ms (95.39%), Space: 8 MB (43.55%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,29 @@
 class Solution {
 private:
     int m, n, wordLen;
-    bool dfs(vector<vector<char>>& board, const string& word, vector<vector<bool>>& visited, int counter,
-             int row, int col, int prevRow, int prevCol) {
-        if (row == -1 || col == -1 || row == m || col == n) return false;
-        if (visited[row][col])
-            return false;
-        visited[row][col] = true;
-        if (board[row][col] == word[counter]) {
-            counter++;
-            if (counter == wordLen) return true;
-            if (prevRow != row + 1 && dfs(board, word, visited, counter,
-            row + 1, col, row, col)) return true;
-            if (prevRow != row - 1 && dfs(board, word, visited, counter,
-            row - 1, col, row, col)) return true;
-            if (prevCol != col + 1 && dfs(board, word, visited, counter,
-                row, col + 1, row, col)) return true;
-            if (prevCol != col - 1 && dfs(board, word, visited, counter,
-                row, col - 1, row, col)) return true;
-            visited[row][col] = false;
-            return false;
-        }
-        visited[row][col] = false;
-        return false;
+    bool dfs(vector<vector<char>>& board, const string& word, int counter,
+             int row, int col) {
+        if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word[counter]) return false;
+        char originalChar = board[row][col];
+        board[row][col] = '#';
+        counter++;
+        if (counter == wordLen) return true;
+        bool found = dfs(board, word, counter, row + 1, col) ||
+                     dfs(board, word, counter, row - 1, col) ||
+                     dfs(board, word, counter, row, col + 1) ||
+                     dfs(board, word, counter, row, col - 1);
+        board[row][col] = originalChar;
+        return found;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
         if (word.empty()) return true;
         if (board.empty()) return false;
         m = board.size(), n = board[0].size(), wordLen = word.size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                if (dfs(board, word, visited, 0,
-                        i, j, -1, -1))
+                if (dfs(board, word, 0,
+                        i, j))
                     return true;
         return false;
     }
