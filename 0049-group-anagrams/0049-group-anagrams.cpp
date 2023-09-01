84d7fb409648710906f67ba3210f83180159ffdf
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-01 17:51:36 +0300

    Time: 24 ms (92.96%), Space: 19.5 MB (85.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        for (const auto & str : strs) {
-            m[getKey(str)].push_back(str);
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        string sortedStr;
+        for (const string& str : strs) {
+            sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());
+            anagramGroups[sortedStr].push_back(str);
         }
-
         vector<vector<string>> result;
-        for (auto & it : m) {
-            result.push_back(it.second);
+        for (const auto& pair : anagramGroups) {
+            result.push_back(pair.second);
         }
         return result;
-    }
-private:
-    string getKey(const string& str) {
-        vector<int> count(26);
-        for (char j : str) {
-            count[j - 'a']++;
-        }
-
-        string key = "";
-        for (int i : count) {
-            key.append(to_string(i) + '#');
-        }
-        return key;
     }
 };
