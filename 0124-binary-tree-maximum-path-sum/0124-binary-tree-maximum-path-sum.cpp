c2cebfdd5cbfc3ce6f8883ededa5071307a4d4e7
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-02 14:06:19 +0300

    Time: 24 ms (52.37%), Space: 33.5 MB (6.24%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 private:
-    int maxPathSum(TreeNode* root, int& maxSum) {
+    int maxPathSum(TreeNode* root, int& maxSum, unordered_map<TreeNode*, int>& dp) {
         if (!root) return 0;
-
+        if (dp.count(root)) return dp[root];
         // Calculate the maximum path sum in the left and right subtrees
-        int leftMax = max(0, maxPathSum(root->left, maxSum));
-        int rightMax = max(0, maxPathSum(root->right, maxSum));
+        int leftMax = max(0, maxPathSum(root->left, maxSum, dp));
+        int rightMax = max(0, maxPathSum(root->right, maxSum, dp));
 
         // Calculate the maximum path sum passing through the current node
         int currentMax = root->val + leftMax + rightMax;
@@ -14,13 +14,14 @@ private:
         maxSum = max(maxSum, currentMax);
 
         // Return the maximum path sum starting from the current node
-        return root->val + max(leftMax, rightMax);
+        return dp[root] = root->val + max(leftMax, rightMax);
     }
 
 public:
     int maxPathSum(TreeNode* root) {
         int maxSum = INT_MIN; // Initialize with the minimum possible value
-        maxPathSum(root, maxSum);
+        unordered_map<TreeNode*, int> dp;
+        maxPathSum(root, maxSum, dp);
         return maxSum;
     }
 };
