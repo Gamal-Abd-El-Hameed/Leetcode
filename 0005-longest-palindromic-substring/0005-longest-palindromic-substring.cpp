20771e1bd5f7207a29c62ba9e38d922e821c64da
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-04 22:38:00 +0300

    Time: 10 ms (89.22%), Space: 6.9 MB (88.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,28 @@
 class Solution {
+private:
+    int subStringStart = 0, maxLen = 0;
+
+    void expandAroundCenter(const string &s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        int len = right - left - 1;
+        if (len > maxLen) {
+            maxLen = len;
+            subStringStart = left + 1;
+        }
+    }
+
 public:
     string longestPalindrome(string s) {
-        int n = s.length(), start = 0, maxLen = 1;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLen = 2;
-            }
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            expandAroundCenter(s, i, i);       // Odd length palindrome
+            expandAroundCenter(s, i, i + 1);   // Even length palindrome
         }
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i < n - len + 1; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    if (len > maxLen) {
-                        maxLen = len;
-                        start = i;
-                    }
-                }
-            }
-        }
-        return s.substr(start, maxLen);
+
+        return s.substr(subStringStart, maxLen);
     }
 };
