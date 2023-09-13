cf93d3a9512b51160037679f493e032a9673492f
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-13 19:21:47 +0300

    Time: 32 ms (21.08%), Space: 23.5 MB (23.23%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root)
-            return nullptr;
-        if (root->val == p->val && findInSubTree(root, q))
-            return root;
-        if (root->val == q->val && findInSubTree(root, p))
-            return root;
-        auto leftSubTree = lowestCommonAncestor(root->left, p, q);
-        if (leftSubTree)
-            return leftSubTree;
-        auto rightSubTree = lowestCommonAncestor(root->right, p, q);
-        if (rightSubTree)
-            return rightSubTree;
-        if (findInSubTree(root->left, p) && findInSubTree(root->right, q))
-            return root;
-        if (findInSubTree(root->left, q) && findInSubTree(root->right, p))
-            return root;
-        return nullptr;
-    }
-private:
-    bool findInSubTree(TreeNode* root, TreeNode* child) {
-        if (!root)
-            return false;
-        if (root->val == child->val)
-            return true;
-        return findInSubTree(root->left, child) || findInSubTree(root->right, child);
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+
+        if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+
+        return root;
     }
 };
