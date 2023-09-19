d6531b1c166bd085d603ee6b10f27f9ffb216151
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-19 18:17:12 +0300

    Time: 132 ms (18.68%), Space: 128.3 MB (25.71%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        if (accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0))
-            return -1;
-        int ans = 0, totalGas = 0;
+        int ans = 0, totalGas = 0, currentGas = 0;
         for (int station = 0; station < gas.size(); station++) {
             totalGas += gas[station] - cost[station];
-            if (totalGas < 0) {
-                totalGas = 0;
+            currentGas += gas[station] - cost[station];
+            if (currentGas < 0) {
+                currentGas = 0;
                 ans = station + 1;
             }
         }
-        return ans;
+        return totalGas >= 0 ? ans : -1;
     }
 };
