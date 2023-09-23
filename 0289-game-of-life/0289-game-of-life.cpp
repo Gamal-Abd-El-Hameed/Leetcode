3a62439fe12b4d0a0391382893204075d4f0a953
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-23 11:37:56 +0300

    Time: 0 ms (100.00%), Space: 7.2 MB (13.67%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -6,37 +6,33 @@ private:
     }
     int getNewValOf(vector<vector<int>>& board, int i, int j) {
         int liveNeighbors = 0;
-        if (isLive(board, i - 1, j - 1))
-            liveNeighbors++;
-        if (isLive(board, i - 1, j))
-            liveNeighbors++;
-        if (isLive(board, i - 1, j + 1))
-            liveNeighbors++;
-        if (isLive(board, i, j - 1))
-            liveNeighbors++;
-        if (isLive(board, i, j + 1))
-            liveNeighbors++;
-        if (isLive(board, i + 1, j - 1))
-            liveNeighbors++;
-        if (isLive(board, i + 1, j))
-            liveNeighbors++;
-        if (isLive(board, i + 1, j + 1))
-            liveNeighbors++;
+        int directions[8][2] = {{-1, -1}, {-1, 0}, {-1, 1},
+                                {0, -1}, {0, 1},
+                                {1, -1}, {1, 0}, {1, 1}};
+        for (auto & direction : directions) {
+            int x = i + direction[0], y = j + direction[1];
+            if (isLive(board, x, y))
+                liveNeighbors++;
+        }
         if (board[i][j]) {
-            if (liveNeighbors < 2 || liveNeighbors > 3)
-                return 0;
-            return 1;
+            return liveNeighbors == 2 || liveNeighbors == 3;
         }
         return liveNeighbors == 3;
     }
 public:
     void gameOfLife(vector<vector<int>>& board) {
         m = board.size(), n = board[0].size();
-        vector<vector<int>> res(m, vector<int>(n));
+        vector<int> prevRow(n, 0), curRow(n, 0);
+        curRow = board[0];
 
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++) {
+            prevRow = curRow;
+            curRow = board[i];
             for (int j = 0; j < n; j++)
-                res[i][j] = getNewValOf(board, i, j);
-        board = res;
+                curRow[j] = getNewValOf(board, i, j);
+            if (i)
+                board[i - 1] = prevRow;
+        }
+        board[m - 1] = curRow;
     }
 };
