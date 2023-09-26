e1a8b70e7d165e3e9023f1496d15a42addab0f20
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-26 20:20:17 +0300

    Time: 9 ms (23.39%), Space: 11.9 MB (15.62%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,39 +1,23 @@
 class Solution {
-private:
-    int getSize(ListNode* head) {
-        int size = 0;
-        auto curr = head;
-        while (curr) {
-            size++;
-            curr = curr->next;
-        }
-        return size;
-    }
-    pair<ListNode*, ListNode*> reverseGroup(ListNode* head, int k) {
-        ListNode *curr = head, *prev = nullptr, *next;
-        while (k--) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return {prev, head};
-    }
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if (!head)
-            return nullptr;
-        int n = getSize(head);
+            return head;
+        int n = 1;
+        auto tail = head;
+        while (tail->next) {
+            n++;
+            tail = tail->next;
+        }
         k = k % n;
         if (k == 0)
             return head;
-        auto pair = reverseGroup(head, n);
-        auto curr = pair.first;
-        for (int i = 0; i < k; i++)
-            curr = curr->next;
-        auto leftPair = reverseGroup(pair.first, k);
-        auto rightPair = reverseGroup(curr, n - k);
-        leftPair.second->next = rightPair.first;
-        return leftPair.first;
+        auto newTail = head;
+        for (int i = 0; i < n - k - 1; i++)
+            newTail = newTail->next;
+        auto newHead = newTail->next;
+        newTail->next = nullptr;
+        tail->next = head;
+        return newHead;
     }
 };
