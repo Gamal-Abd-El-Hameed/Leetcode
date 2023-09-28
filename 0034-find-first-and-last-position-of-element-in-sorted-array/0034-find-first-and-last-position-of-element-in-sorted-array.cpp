292b2e27cba3707d1a4aa1c51e474ba232cc5bb7
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-28 17:53:20 +0300

    Time: 7 ms (51.19%), Space: 13.9 MB (15.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,41 @@
 class Solution {
 private:
-    int lowerBoundOf(vector<int>& nums, int target) {
-        int n = nums.size(), low = 0, high = n - 1, mid, num;
+    int getLowerBound(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, ans = -1, mid, num;
         while (low <= high) {
             mid = (low + high) >> 1;
             num = nums[mid];
-            if (num == target) {
-                if (mid == 0 || nums[mid - 1] != num)
-                    return mid;
-                high = mid;
-            }
-            else if (num > target)
+            if (num >= target) {
                 high = mid - 1;
-            else
+                if (num == target)
+                    ans = mid;
+            } else {
                 low = mid + 1;
+            }
         }
-        return -1;
+        return ans;
     }
-    int upperBoundOf(vector<int>& nums, int target) {
-        int n = nums.size(), low = 0, high = n - 1, mid, num;
+    int getUpperBound(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, ans = -1, mid, num;
         while (low <= high) {
             mid = (low + high) >> 1;
             num = nums[mid];
-            if (num == target) {
-                if (mid == n - 1 || nums[mid + 1] != num)
-                    return mid;
-                low = mid;
-            }
-            if (num > target)
-                high = mid - 1;
-            else
+            if (num <= target) {
                 low = mid + 1;
+                if (num == target)
+                    ans = mid;
+            } else {
+                high = mid - 1;
+            }
         }
-        return -1;
+        return ans;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lowerBound = lowerBoundOf(nums, target);
+        int lowerBound = getLowerBound(nums, target);
         if (lowerBound == -1)
             return {-1, -1};
-        int upperBound = upperBoundOf(nums, target);
+        int upperBound = getUpperBound(nums, target);
         return {lowerBound, upperBound};
     }
 };
