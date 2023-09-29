d55aaf1c79b5a9ec9bfda65ad62140a5d78123e6
Gamal Abdel Hamid <gamalabdelhameed314@gmail.com>
2023-09-29 21:08:26 +0300

    Time: 3 ms (84.82%), Space: 7.4 MB (61.76%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         this->n = n;
-        vector<int> queenCols(n, -1);
-        vector<vector<string>> res;
-        solveNQueens(queenCols, 0, res);
-        return res;
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> solutions;
+        findSolutions(board, 0, solutions);
+        return solutions;
     }
 private:
     int n;
-    void solveNQueens(vector<int> &queenCols, int row, vector<vector<string>> &res) {
-        if (row == n) {
-            vector<string> board(n, string(n, '.'));
-            for (int queenRow = 0; queenRow < n; queenRow++)
-                board[queenRow][queenCols[queenRow]] = 'Q';
-            res.push_back(board);
-            return;
-        }
-        for (int col = 0; col < n; col++) {
-            if (isSafe(queenCols, row, col)) {
-                queenCols[row] = col;
-                solveNQueens(queenCols, row + 1, res);
-                queenCols[row] = -1;
+    void findSolutions(vector<string> &board, int row, vector<vector<string>> &solutions) {
+        if (row == n)
+            return solutions.push_back(board);
+        for (int col = 0; col < n; col++)
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                findSolutions(board, row + 1, solutions);
+                board[row][col] = '.';
             }
-        }
     }
-    bool isSafe(vector<int> &queenCols, int currRow, int currCol) {
-        for (int queenRow = 0; queenRow < currRow; queenRow++) {
-            int queenCol = queenCols[queenRow];
-            if (queenCol == currCol || currRow - queenRow == abs(currCol - queenCol))
+    bool isValid(vector<string> &board, int row, int col) {
+        for (int i = row - 1; i >= 0; i--)
+            if (board[i][col] == 'Q')
                 return false;
-        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
         return true;
     }
 };
